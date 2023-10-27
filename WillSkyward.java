a6058b9546ac503c05d9e7b2d2f2c55f52a579d3
NeevZeroni <149200557+NeevZeroni@users.noreply.github.com>
2023-10-27 14:29:41 -0500

    Create WillSkyward.java

---
+++ WillSkyward.java
@@ -0,0 +1,12 @@
+public class WillSkyward{
+  public static void main(String [] args){
+    String school[];
+    String name;
+    for ( int students = 0, answer = 0; students !=1; System.out.println("Would you like to add another student?")){
+      System.out.println("What is the name of student "+(students+1)+"?"),
+      School[student] = inputDevice.nextString();
+      System.out.println("Would you like to add another student?");
+      answer = inputDevice.nextInt();
+    }
+  }
+}  
