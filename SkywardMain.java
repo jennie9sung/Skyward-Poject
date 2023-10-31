25c10d567adfde39470cf6fd3941096e4e2a6946
jennie9sung <146058515+jennie9sung@users.noreply.github.com>
2023-10-31 15:01:24 -0500

    Update and rename SkywardMain to SkywardMain.java

---
+++ SkywardMain.java
@@ -0,0 +1,18 @@
+//Main{
+  //print data
+  //give options
+  //options switch to methods in Student
+  //after method is done loop.
+  
+import java.util.Scanner;
+
+public class SkywardMain {
+
+	public static void main(String[] args) {
+		SkywardStudent stu = new SkywardStudent();
+		stu.options();
+
+	}
+
+}
+
