1ded3ab3f7e2f511a266f5f26d467915f0cfbfe8
jennie9sung <146058515+jennie9sung@users.noreply.github.com>
2023-10-31 15:00:09 -0500

    Update JenniferSkyward.java

--- JenniferSkyward.java
+++ JenniferSkyward.java
@@ -1,5 +1,25 @@
 import java.util.Scanner;
 
-public class CircleProperties {
+public class JenniferSkyward {
+
 	public static void main(String[] args) {
-    
+		options();
+
+	}
+	public static void options() {
+		Scanner reader = new Scanner(System.in);
+		// pring out options
+		System.out.println("1. Add class");
+		System.out.println("2. Add student");
+		System.out.println("3. Assign class");
+		System.out.println("4. Assign grade");
+		System.out.println("5. Edit Attendance");
+		System.out.println("");
+		System.out.print("Choose an option: ");
+		// user input
+		int choice = reader.nextInt();
+		
+		
+	}
+
+}
