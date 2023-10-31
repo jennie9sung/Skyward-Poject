c306d922443d667c4066e6e4f54748a1569b388c
jennie9sung <146058515+jennie9sung@users.noreply.github.com>
2023-10-31 15:00:51 -0500

    Update JenniferSkyward.java

--- JenniferSkyward.java
+++ JenniferSkyward.java
@@ -1,25 +1,11 @@
 import java.util.Scanner;
 
-public class JenniferSkyward {
+public class SkywardMain {
 
 	public static void main(String[] args) {
-		options();
+		SkywardStudent stu = new SkywardStudent();
+		stu.options();
 
-	}
-	public static void options() {
-		Scanner reader = new Scanner(System.in);
-		// pring out options
-		System.out.println("1. Add class");
-		System.out.println("2. Add student");
-		System.out.println("3. Assign class");
-		System.out.println("4. Assign grade");
-		System.out.println("5. Edit Attendance");
-		System.out.println("");
-		System.out.print("Choose an option: ");
-		// user input
-		int choice = reader.nextInt();
-		
-		
 	}
 
 }
