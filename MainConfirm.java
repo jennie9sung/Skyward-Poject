42b6c2709d3e4dff005f1c1f9e18cd38cc7cc937
TacomanBalls <149200588+TacomanBalls@users.noreply.github.com>
2023-11-01 16:51:15 -0500

    Update MainConfirm.java

--- MainConfirm.java
+++ MainConfirm.java
@@ -1,27 +1,30 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
+import java.util.ArrayList
 public class SkywardMain {
 
 	public static void main(String[] args) {
-		SkywardStudent stu = new SkywardStudent();
-		options();
-
-	}
-	
-	public static void options() {
-		// call class
-		SkywardStudent stu = new SkywardStudent();
+		
 		
 		Scanner reader = new Scanner(System.in);
 		
+		int studenttotal=0;
+		ArrayList<String> Students = new ArrayList<String>();
+		
+		int choice;
+		do {
+		
+	    System.out.println("====================Skyward====================="");    
+		System.out.println("\t" + Students.get(0));
+		    
+		    
 		// pring out options
 		System.out.println("1. Add a student");
 		System.out.println("2. Enroll a student");
 		System.out.println("3. Assign course");
 		System.out.println("4. Assign grade");
 		System.out.println("5. Edit Attendance");
-		System.out.println("");
+		System.out.println("6. Create a Class");
+		System.out.println("7. Exit");
 		System.out.print("Choose an option: ");
 		// user input
 		int choice = reader.nextInt();
@@ -29,27 +32,29 @@ public class SkywardMain {
         // switch statement here?
 	    switch(choice){
 		    case 1:
-//		    	if (courses[5]!=0) {
-//			   System.out.print("Course name: "); 
-//			   String course = reader.next();
-//			   System.out.println("");
-//			   for (int n=0;n<5;n++) {
-//				   if ()
-//			   }
-//		    	}else {
-//		    		System.out.println("You reached the maximum number of courses.");
-//		    		options();
-//		    	}
-		    	System.out.println("?");
-		    	options();
+		    	System.out.println("Enter the student's name");
+		    	String name = reader.nextLine();
+		    	Students.add(name);
+		    	SkywardStudent studenttotal  = new SkywardStudent();
 		    	break;
 		    case 2:
 			   System.out.println("Type the class ID: ");
 			   int id = reader.nextInt();
-			   stu.enrollStudent(id);
-			   System.out.print(stu.student[1][1]);
-			   options();
+			   break; 
+		    case 3:
+		        break;
+		    case 4:
+		        break;
+		    case 5:
+		        break;
+		    case 6:
+		        break;
+		    case 7:
+		        System.exit(0);
+		        break;
 	    }
+	    }
+	    while (choice != 5);
 	}
 
 }
