a0cfb269c56d72c80ea742993c35ef97b8f9cd33
jennie9sung <146058515+jennie9sung@users.noreply.github.com>
2023-11-03 16:35:03 +0900

    Update MainConfirm.java
    added "Assign Class" to a certain student

--- MainConfirm.java
+++ MainConfirm.java
@@ -8,21 +8,30 @@ public class SkywardMain {
 		Scanner reader = new Scanner(System.in);
 		
 		int studenttotal=0;
+		int classnum =0;
 		ArrayList<String> Students = new ArrayList<String>();
+		ArrayList<String> Classes = new ArrayList<String>();
 		
 		int choice;
 		do {
-		
+		System.out.println("");
 	    System.out.println("====================Skyward=====================");    
 		for(int i=0; i<studenttotal; i++){
-		    System.out.println("\t" + (i+1) + ". " + Students.get(i));
+			// this one's to check variable values --> remove this part later
+			System.out.println(studenttotal+"  "+classnum);
+			
+		    System.out.print("\t" + (i+1) + ". " + Students.get(i));
+		    if (classnum>0&&i<classnum) {
+		    	System.out.println("    " + Classes.get(i));
+		    }
+		    System.out.println("");
 		    
 		}    
 		System.out.println("\n------------------------------------------------");    
-		// pring out options
+		// print out options
 		System.out.println("1. Add a student");
-		System.out.println("2. Enroll a student");
-		System.out.println("3. Assign course");
+		//System.out.println("2. Enroll a student");
+		System.out.println("3. Assign course"); // is assign course adding new courses?(Jennifer)
 		System.out.println("4. Assign grade");
 		System.out.println("5. Edit Attendance");
 		System.out.println("6. Create a Class");
@@ -34,17 +43,26 @@ public class SkywardMain {
         // switch statement here?
 	    switch(choice){
 		    case 1:
-		    	System.out.println("Enter the student's name");
+		    	System.out.println("Enter the student's name: ");
 		    	String name = reader.next();
 		    	Students.add(name);
+		    	Classes.add("");
 //		    	SkywardStudent studenttotal  = new SkywardStudent();
 		    	studenttotal++;
+		    	classnum++;
+		    	System.out.println("The Student ID for "+name+" is "+studenttotal+".");
 		    	break;
 		    case 2:
-			   System.out.println("Type the class ID: ");
-			   int id = reader.nextInt();
 			   break; 
 		    case 3:
+		    	// assign class to a certain student (id given when add new student)
+		    	// worked when I tested
+		    	System.out.println("Enter the class name: ");
+				   String classname = reader.next();
+				   System.out.println("Enter the Student ID: ");
+				   int id = reader.nextInt();
+				   Classes.set(id-1, classname);
+				   classnum++;
 		        break;
 		    case 4:
 		        break;
@@ -56,7 +74,7 @@ public class SkywardMain {
 		        System.exit(0);
 		        break;
 	    }
-	    System. out. print("\033[H\033[2J");
+	    System. out. print("\033[H\033[2J"); // what is this doing? I thought it was clearing smt (Jennifer)
 	    }
 	    while (choice != 5);
 	}
