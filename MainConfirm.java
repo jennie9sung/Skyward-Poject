6a077dc83a8d9cf6e6430fe3a45b3667288e5b9a
TacomanBalls <149200588+TacomanBalls@users.noreply.github.com>
2023-11-04 11:46:48 -0500

    Update MainConfirm.java

--- MainConfirm.java
+++ MainConfirm.java
@@ -19,8 +19,12 @@ public class SkywardMain {
 		for(int i=0; i<studenttotal; i++){
 		    System.out.println("\t" + (i+1) + ". " + Students.get(i));
 		    int c = school.getClasstotal(i);
-		    for(int k = 0; k<c; k++) 
-		        System.out.println("\t\t" + Classes.get(k));
+		    for(int k = 0; k<c; k++){ 
+		        System.out.print("\t\t" + Classes.get(k));
+		        //print out grade then attendace for class k
+		        System.out.print("(Grade: " + school.getGrade(i,k) + ", Attendace: " + school.getAttendance(i,k) + ")");
+		    }
+		    System.out.println("");
 		}    
 		
 		System.out.println("\n------------------------------------------------");
@@ -33,12 +37,11 @@ public class SkywardMain {
 		System.out.println("================================================");    
 		// print out options
 		System.out.println("1. Add a student");
-		System.out.println("2. Enroll a student");
-		System.out.println("3. Assign course to a student");
-		System.out.println("4. Assign grade to a student's class");
-		System.out.println("5. Edit a student's Attendance");
-		System.out.println("6. Create a Class");
-		System.out.println("7. Exit");
+		System.out.println("2. Enroll a student in a class");
+		System.out.println("3. Assign grade to a student's class");
+		System.out.println("4. Edit a student's Attendance");
+		System.out.println("5. Create a Class");
+		System.out.println("6. Exit");
 		System.out.print("Choose an option: ");
 		// user input
 	    choice = reader.nextInt();
@@ -59,22 +62,35 @@ public class SkywardMain {
 		       school.enrollStudent(studentID,classID);
 			   break; 
 		    case 3:
+		        System.out.print("Enter the student's ID:");
+		        studentID=reader.nextInt()-1;
+		        System.out.print("Enter the class's ID: ");
+		        classID=reader.nextInt();
+                System.out.print("Enter the percent grade you would like to change to: ");	
+                int grade = reader.nextInt();
+                school.changeGrade(studentID, grade, classID);
 		        break;
 		    case 4:
+		        System.out.print("Enter the student's ID:");
+		        studentID=reader.nextInt()-1;
+		        System.out.print("Enter the class's ID: ");
+		        classID=reader.nextInt();
+                System.out.print("Enter the number of tardys the student has in this class:");	
+                int attendance = reader.nextInt();
+                school.changeAttendance(studentID, attendance, classID);
 		        break;
 		    case 5:
-		        break;
-		    case 6:
 		        System.out.println("Enter the class's name: ");
 		    	String classname = reader.next();
 		    	Classes.add(classname);
 		    	classtotal++;
 		    	break;
-		    case 7:
+		    case 6:
 		        System.exit(0);
 		        break;
 	    }
-	    System. out. print("\033[H\033[2J"); 
+	    System.out.print("\033[H\033[2J"); 
+	    System.out.flush();
 	    }
 	    while (choice != 5);
 	}
