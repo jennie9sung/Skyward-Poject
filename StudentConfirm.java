a9e509d99fa2f15b56887e54d872d253711446d2
TacomanBalls <149200588+TacomanBalls@users.noreply.github.com>
2023-11-04 11:48:05 -0500

    Update StudentConfirm.java

--- StudentConfirm.java
+++ StudentConfirm.java
@@ -1,62 +1,81 @@
 import java.util.Scanner;
-public class SkywardStudent{
-    private int classes;
-    private int students;
-    public int[][][] school;
-    
-    public int choice;
-    
-    private int[] courses = new int[5];
 
-    // constructor that also initializes the array for the student
-    public SkywardStudent(){ //All of the methods are public by default, but as of now they are not static. Is that a purposeful choice?
+public class SkywardStudent {
+    private int classes; // Variable to track the number of classes a student is enrolled in
+    private int students; // Variable to track the number of students (currently not used)
+    private int[][][] school; // 3D array to store student information: [studentID][classIndex][0:classID, 1:grade, 2:attendance]
+
+    // Constructor to initialize the class properties and the school array
+    public SkywardStudent() {
         classes = 0;
-        students=0;
-        int[][][] school = new int[10][3][2];
+        students = 0;
+        // Initializing the school array with dimensions [10][3][2] for 10 students, 3 classes per student, and 2 data points (classID, grade, attendance)
+        school = new int[10][3][2];
     }
 
-    public void givebirth(int studentID){
-        
-    }
-    
+    // Method to enroll a student in a class
     public void enrollStudent(int studentID, int classID) {
-        school[studentID][classes][0] = classID;
-        classes++;
+        if (classes < 3) { // Checking if the student can enroll in more classes (maximum 3 classes allowed)
+            school[studentID][classes][0] = classID; // Storing the classID in the school array
+            classes++; // Incrementing the classes count for the current student
+        } else {
+            System.out.println("Student can only enroll in 3 classes."); // Printing a message if the maximum class limit is reached
+        }
     }
-    
-    public void changeGrade (int studentID, int grade, int classID){
-        int var1=0;
-        for(int var2=0; var2<classes;var2++){ //What does this do? --Will  //for loop to match inputted class ID with a row in the student's array -Neev 
-            if(school[studentID][var2][0]==classID){
-                var1=var2;
+
+    // Method to change the grade for a specific class of a student
+    public void changeGrade(int studentID, int grade, int classID) {
+        for (int i = 0; i < classes; i++) {
+            if (school[studentID][i][0] == classID) { // Finding the class with the given classID for the student
+                school[studentID][i][1] = grade; // Updating the grade for that class
+                return;
             }
         }
-        school[studentID][var1][2]=grade;
+        System.out.println("Class ID not found for the student."); // Printing a message if the classID is not found
     }
-    
-    public void changeAttendance (int studentID, int attendance, int classID){
-        int var1=0; //Why not declare var1 in the class as opposed to declaring it in a method? //I mean we could but it might get mixed up with the other var1 in the other method. I mean this should work but if you want to make it better go for it -Neev
-        for(int var2=0; var2<classes;var2++){
-            if(school[studentID][var2][0]==classID){
-                var1=var2;
+
+    // Method to change the attendance for a specific class of a student
+    public void changeAttendance(int studentID, int attendance, int classID) {
+        for (int i = 0; i < classes; i++) {
+            if (school[studentID][i][0] == classID) { // Finding the class with the given classID for the student
+                school[studentID][i][2] = attendance; // Updating the attendance for that class
+                return;
             }
-        school[studentID][var1][1]=attendance;
         }
+        System.out.println("Class ID not found for the student."); // Printing a message if the classID is not found
+    }
+
+    // Method to get the total number of classes a student is enrolled in
+    public int getClasstotal(int studentID) {
+        int classtotal = 0; // Variable to store the total number of classes
+        for (int i = 0; i < 3; i++) { // Looping through the classes (maximum 3)
+            if (school[studentID][i][0] != 0) { // Checking if the classID is not zero (indicating a valid class)
+                classtotal++; // Incrementing the total classes count
+            }
+        }
+        return classtotal; // Returning the total number of classes the student is enrolled in
     }
     
-    public static int getClasstotal (int studentID){
-        int classtotal=0;
-        for (int i=0; i<4; i++){
-            if(school[studentID][i][0] != 0){
-                classtotal++;
-            } 
+    public int getAttendance(int studentID, int classID) {
+        for (int i = 0; i < classes; i++) {
+            if (school[studentID][i][0] == classID) { // Finding the class with the given classID for the student
+                return school[studentID][i][2]; // Returning the attendance for that class
+            }
         }
-        return classtotal;
+        System.out.println("Class ID not found for the student."); // Printing a message if the classID is not found
+        return -1; // Returning -1 to indicate that the classID was not found
     }
     
-    
-    
+    // Method to get the grade for a specific class of a student
+    public int getGrade(int studentID, int classID) {
+        for (int i = 0; i < classes; i++) {
+            if (school[studentID][i][0] == classID) { // Finding the class with the given classID for the student
+                return school[studentID][i][1]; // Returning the grade for that class
+            }
+        }
+        System.out.println("Class ID not found for the student."); // Printing a message if the classID is not found
+        return -1; // Returning -1 to indicate that the classID was not found
+    }
     
     
 }
-    
