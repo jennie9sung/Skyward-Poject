f43ccc9e6db82c8697ed9b2bf841097642b4e056
TacomanBalls <149200588+TacomanBalls@users.noreply.github.com>
2023-11-05 16:26:23 -0600

    Update SkywardStudent.java

--- SkywardStudent.java
+++ SkywardStudent.java
@@ -1,57 +1,79 @@
-public class SkywardStudent{
-    private int grade; 
-    private int attendance;
-    private int classes;
+import java.util.Scanner;
 
-    // constructor?
-    public SkywardStudent(){ //All of the methods are public by default, but as of now they are not static. Is that a purposeful choice?
+public class SkywardStudent {
+    private int classes; // Variable to track the number of classes a student is enrolled in
+    private int students; // Variable to track the number of students (currently not used)
+    private int[][][] school; // 3D array to store student information: [studentID][classIndex][0:classID, 1:grade, 2:attendance]
+
+    // Constructor to initialize the class properties and the school array
+    public SkywardStudent() {
         classes = 0;
-        int[][] student = new int[3][2];
+        students = 0;
+        // Initializing the school array with dimensions [10][3][2] for 10 students, 3 classes per student, and 2 data points (classID, grade, attendance)
+        school = new int[10][3][2];
     }
 
-    // print options + get user input
-    public void options() {
-		// call class
-		Scanner reader = new Scanner(System.in);
-		
-		// pring out options
-		System.out.println("1. Add school");
-		System.out.println("2. Add student");
-		System.out.println("3. Assign class");
-		System.out.println("4. Assign grade");
-		System.out.println("5. Edit Attendance");
-		System.out.println("");
-		System.out.print("Choose an option: ");
-		// user input
-		int choice = reader.nextInt();
+    // Method to enroll a student in a class
+    public void enrollStudent(int studentID, int classID) {
+        if (getClasstotal(studentID) < 4) { // Checking if the student can enroll in more classes (maximum 3 classes allowed)
+            school[studentID][getClasstotal(studentID)][0] = classID; // Storing the classID in the school array
+        } else {
+            System.out.println("Student can only enroll in 3 classes."); // Printing a message if the maximum class limit is reached
+        }
+    }
 
-        // switch statement here?
-	}
-    
-    public void enrollStudent(int classID) {
-        student[classes][0] = classID;
-        classes++;
-    }
-    
-    public void changeGrade (int grade, int classID){
-        int var1;
-        for(int var2=0; var2<classes;var2++){ //What does this do? --Will 
-            if(student[var2][0]==classID){
-                var1=var2;
+    // Method to change the grade for a specific class of a student
+    public void changeGrade(int studentID, int grade, int classID) {
+        for (int i = 0; i < getClasstotal(studentID); i++) {
+            if (school[studentID][i][0] == classID) { // Finding the class with the given classID for the student
+                school[studentID][i][1] = grade; // Updating the grade for that class
+                return;
             }
         }
-        student[var1][2]=grade;
     }
-    
-    public void changeAttendance (int attendance, int classID){
-        int var1; //Why not declare var1 in the class as opposed to declaring it in a method?
-        for(int var2=0; var2<classes;var2++){
-            if(student[var2][0]==classID){
-                var1=var2;
+
+    // Method to change the attendance for a specific class of a student
+    public void changeAttendance(int studentID, int attendance, int classID) {
+        for (int i = 0; i < getClasstotal(studentID); i++) {
+            if (school[studentID][i][0] == classID) { // Finding the class with the given classID for the student
+                school[studentID][i][2] = attendance; // Updating the attendance for that class
+                return;
             }
         }
-        student[var1][1]=attendance;
+    }
+
+    // Method to get the total number of classes a student is enrolled in
+    public int getClasstotal(int studentID) {
+        int classtotal = 0; // Variable to store the total number of classes
+        for (int i = 0; i < 3; i++) { // Looping through the classes (maximum 3)
+            if (school[studentID][i][0] != 0) { // Checking if the classID is not zero (indicating a valid class)
+                classtotal++; // Incrementing the total classes count
+            }
+        }
+        return classtotal; // Returning the total number of classes the student is enrolled in
     }
     
+    public int getAttendance(int studentID, int classID) {
+        for (int i = 0; i < getClasstotal(studentID); i++) {
+            if (school[studentID][i][0] == classID) { // Finding the class with the given classID for the student
+                return school[studentID][i][2]; // Returning the attendance for that class
+            }
+        }
+        return 0;
+    }
+    
+    // Method to get the grade for a specific class of a student
+    public int getGrade(int studentID, int classID) {
+        for (int i = 0; i < getClasstotal(studentID); i++) {
+            if (school[studentID][i][0] == classID) { // Finding the class with the given classID for the student
+                return school[studentID][i][1]; // Returning the grade for that class
+            }
+        }
+        return 0;
+    }
+    
+    public int getClassID(int studentID, int k){
+        return school[studentID][k][0];
+    }
     
 }
